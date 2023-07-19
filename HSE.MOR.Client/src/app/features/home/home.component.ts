a3aa702b6fff225064e0ceef42ca4fe2878aa6f6
Martin Bloomfield <martin.bloomfield@hse.gov.uk>
2023-07-19 15:12:43 +0100

    Added MOR start page content #4346

--- HSE.MOR.Client/src/app/features/home/home.component.ts
+++ HSE.MOR.Client/src/app/features/home/home.component.ts
@@ -9,7 +9,7 @@ import { TitleService } from '../../services/title.service';
 export class HomeComponent {
   public static route: string = "";
   continueLink: string = '/'
-  static title: string = "Mandatory Occurrence Reporting";
+  static title: string = "Make a mandatory occurrence report";
 
   constructor(private applicationService: ApplicationService, private router: Router, private titleService: TitleService) {    
   }
